b419b001b706024a0b2e437154f8f86da4cc27fd
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2022-03-08 14:40:21 -0300

    Update telegramBot.py

--- src/telegramBot.py
+++ src/telegramBot.py
@@ -1,6 +1,8 @@
 from msilib.schema import Class
+from urllib import request
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -9,3 +11,7 @@ class TelegramBot:
     def __init__(self):
         TOKEN = os.getenv("API_KEY")
         self.url = f"https://api.telegram.org/bot{TOKEN}/"
+    
+    def get_message(self):
+        link_request = f"{self.url}getUpdate?timeout=1000"
+        result = request.get(link_request)

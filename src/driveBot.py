1d2c646c05e0acfbfd4fc42ed0271136dea54936
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2022-03-22 14:02:46 -0300

    Seguraça da key no env

--- src/driveBot.py
+++ src/driveBot.py
@@ -10,6 +10,7 @@ class driveBot:
         self.gc = gspread.service_account(filename = "credentials.json")
 
     def get_data(self):
-        sh = self.gc.open_by_key("1ct7p9CuQpuQK3860SiQup0vD6yyZEioKcKgyGM3bWhI")
+        link_google_sheet = os.getenv("LINK_SHEET")
+        sh = self.gc.open_by_key(link_google_sheet)
         worksheet = sh.sheet1
         return worksheet.get_all_values()
